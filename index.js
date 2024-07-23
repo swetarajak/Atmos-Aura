2c9547ca71cd15678cc3e34c27e2c0a2ddb95cf0
swetarajak <swetarajak001@gmail.com>
2024-07-24 00:30:47 +0530

    Intial code added

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const axios = require('axios');
+const app = express();
+require('dotenv').config();
+
+app.set('view engine', 'ejs');
+const API_KEY = process.env.API_KEY;
+// app.get('/',(req,res) =>{
+//     res.send("Hello World");
+// })
+
+app.get("/", function(req,res){
+    const place = req.query.place
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${place}&appid=${API_KEY}`
+
+    axios.get(url).then(response => {
+        const data = response.data;
+        // console.log(data)
+        const cityName = data.name;
+        const temperature = data.main.temp;
+        const message = `City Name: ${cityName} <br> Temperature: ${temperature}`
+        res.send(`<html><body><div id='container'><h1>${message}</h1></div></body></html>`)
+    })
+    .catch(error => {
+        console.log(error);
+    })
+});
+
+app.listen(3000, function() {
+    console.log("Server is running");
+})
